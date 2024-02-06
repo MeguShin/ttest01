b0fd92c75cda5b785e715b4915566ae198f92332
megu <megushin0715@gmail.com>
2024-02-07 00:04:16 +0900

    add nginx and unicorn

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.10"    # 1
+lock "~> 3.18.0"
 
 set :application, "ttest01"
 set :repo_url, "https://github.com/MeguShin/ttest01.git"
@@ -8,6 +8,7 @@ set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets public/uploads}   # 5
 set :keep_releases, 5   # 6
 set :rbenv_ruby, '3.0.1'    # 7
 set :log_level, :info   # 8
+append :linked_files, 'config/secrets.yml'
 
 after 'deploy:published', 'deploy:seed'   # 9
 after 'deploy:finished', 'deploy:restart'   # 10
@@ -18,7 +19,7 @@ task :seed do
     on roles(:db) do
     with rails_env: fetch(:rails_env) do
         within current_path do
-        execute :bundle, :exec, :rake, 'db:seed'
+        execute :bundle, :exec, :rake
         end
     end
     end
